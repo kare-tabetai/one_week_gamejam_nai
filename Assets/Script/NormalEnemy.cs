2907e8187421dc215f32279d1235c198fd3f61bc
ti_kare_ <yahoo.zip.lzh.exe@gmailcom>
2024-12-28 01:24:46 +0900

    敵の攻撃処理を作成中

--- Assets/Script/NormalEnemy.cs
+++ Assets/Script/NormalEnemy.cs
@@ -2,21 +2,117 @@ using UnityEngine;
 
 public class NormalEnemy : MonoBehaviour
 {
+    public enum State
+    {
+        Idle,
+        TrackingToShootPoint,
+        TrackingToAttackPoint,
+        Shooting,
+        ShootingInterval,
+        Attacking,
+        AttackingInterval,
+    }
+
     public Animator animator;
+    public Rigidbody rb;
     public int hp = 100;
     public GameObject brick;
     public GameObject brick_prefab;
+    public float brick_shoot_range;
+    public float attack_range;
+    public Transform target;
+    public float tracking_speed;
+    public float throw_up_y_max = 1.0f;
 
-    void Start()
+    State state;
+    float timer;
+
+    void Update()
+    {
+        if (hp <= 0) { return; }
+
+        switch (state)
+        {
+            case State.Idle:
+                UpdateIdle();
+                break;
+            case State.TrackingToShootPoint:
+                UpdateShootPointTracking();
+                break;
+            case State.TrackingToAttackPoint:
+                UpdateAttackPointTracking();
+                break;
+            case State.Shooting:
+                UpdateShooting();
+                break;
+            case State.Attacking:
+                UpdateAttacking();
+                break;
+        }
+
+    }
+
+    void UpdateIdle()
+    {
+        if (target)
+        {
+            state = Random.Range(0, 2) == 0 ? State.TrackingToShootPoint : State.TrackingToAttackPoint;
+            animator.SetBool("Walking", true);
+        }
+        else
+        {
+            animator.SetBool("Walking", false);
+        }
+    }
+
+    void UpdateShootPointTracking()
+    {
+        var to_target = target.position - transform.position;
+        if (to_target.sqrMagnitude <= brick_shoot_range * brick_shoot_range)
+        {
+            ChangeStateShoot();
+            return;
+        }
+
+        to_target.Normalize();
+        rb.velocity = Vector3.zero;
+    }
+
+    void ChangeStateShoot()
+    {
+        state = State.Shooting;
+        animator.SetTrigger("Shoot");
+        rb.velocity = Vector3.zero;
+    }
+
+    void UpdateAttackPointTracking()
+    {
+        var to_target = target.position - transform.position;
+        if (to_target.sqrMagnitude <= attack_range * attack_range)
+        {
+            ChangeStateAttack();
+            return;
+        }
+
+        to_target.Normalize();
+        rb.velocity = Vector3.zero;
+    }
+
+    void ChangeStateAttack()
+    {
+        state = State.Attacking;
+        animator.SetTrigger("Attack");
+        rb.velocity = Vector3.zero;
+    }
+
+    void UpdateShooting()
     {
 
     }
 
-    void Update()
+    void UpdateAttacking()
     {
-        bool walking = false;
-        //TODO:
-        animator.SetBool("Walking", walking);
+
     }
 
     public void Damaged(int damage)
@@ -36,10 +132,28 @@ public class NormalEnemy : MonoBehaviour
 
     public void ShotBrick()
     {
+        if (!target) { return; }
+
         brick.SetActive(false);
         var brick_shot = Instantiate(
             brick_prefab,
             brick.transform.position,
             Quaternion.identity);
+
+        var dir = ThrowUpCalculation.OrbitCalculations(
+            brick_shot.transform.position,
+            target.position,
+            target.position.y + throw_up_y_max);
+        brick_shot.GetComponent<Bullet>().Initialize(dir);
+    }
+
+    public void OnEnterPlayer(Collider2D col)
+    {
+        target = col.transform;
+    }
+
+    public void OnExitPlayer(Collider2D col)
+    {
+        target = null;
     }
 }
---
+++ Assets/Script/TriggerEvent.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TriggerEvent : MonoBehaviour {
+
+    [System.Serializable]
+    private class TriggerUnityEvent : UnityEvent<Collider2D> { }
+
+    [SerializeField]
+    TriggerUnityEvent m_enter_events;
+    [SerializeField]
+    TriggerUnityEvent m_stay_events;
+    [SerializeField]
+    TriggerUnityEvent m_exit_events;
+
+    [SerializeField]
+    string[] m_trigger_tag;
+
+    public void DestroyGameObject(Collider2D collision)
+    {
+        if (collision.gameObject)
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (m_enter_events == null)
+        {
+            return;
+        }
+
+        if (m_trigger_tag.Length == 0)
+        {
+            m_enter_events?.Invoke(collision);
+            return;
+
+        }
+
+        foreach (var str in m_trigger_tag)
+        {
+            if (collision.CompareTag(str))
+            {
+                m_enter_events?.Invoke(collision);
+                return;
+            }
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (m_exit_events == null)
+        {
+            return;
+        }
+
+        if (m_trigger_tag.Length == 0)
+        {
+            m_stay_events?.Invoke(collision);
+            return;
+        }
+
+        foreach (var str in m_trigger_tag)
+        {
+            if (collision.CompareTag(str))
+            {
+                m_stay_events?.Invoke(collision);
+                return;
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (m_stay_events == null)
+        {
+            return;
+        }
+
+        if (m_trigger_tag.Length == 0)
+        {
+            m_exit_events?.Invoke(collision);
+            return;
+        }
+
+        foreach (var str in m_trigger_tag)
+        {
+            if (collision.CompareTag(str))
+            {
+                m_exit_events?.Invoke(collision);
+                return;
+            }
+        }
+    }
+}
